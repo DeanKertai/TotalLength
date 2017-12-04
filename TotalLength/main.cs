bd506302525ad10ce72d19ebf9aafb70c9725288
DeanKertai <deankertai@gmail.com>
2017-12-04 10:57:40 -0600

    initial commit

---
+++ TotalLength/main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+[assembly: CommandClass(typeof(TotalLength.Main))]
+
+namespace TotalLength
+{
+
+    public class Main
+    {
+
+        [CommandMethod("TotalLength", CommandFlags.Modal | CommandFlags.UsePickSet)]
+        public void command()
+        {
+            // Get current selection set from the AutoCAD editor
+            PromptSelectionResult result = Application.DocumentManager.MdiActiveDocument.Editor.GetSelection();
+            if (result.Status == PromptStatus.OK)
+            {
+                
+            }
+            
+        }
+
+    }
+
+}
---
+++ TotalLength/plugin.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+[assembly: ExtensionApplication(typeof(TotalLength.Plugin))]
+
+namespace TotalLength
+{
+    /**
+     * This class is instantiated by AutoCAD once and kept alive for the 
+     * duration of the session
+     */
+    public class Plugin : IExtensionApplication
+    {
+
+        void IExtensionApplication.Initialize()
+        {
+            
+        }
+
+        void IExtensionApplication.Terminate()
+        {
+            
+        }
+
+    }
+}
